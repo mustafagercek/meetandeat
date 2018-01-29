6ea1813ba94dbd1113c4a72ef76d06436878ab41
Mustafa Gercek <mgercek92@googlemail.com>
2018-01-29 19:26:33 +0100

    noshow

--- app/controllers/api/noshows_controller.rb
+++ app/controllers/api/noshows_controller.rb
@@ -2,18 +2,18 @@ class Api::NoshowsController < BaseApiController
 
 
   def create
+    #Check if current user was invited
     @noshow = Noshow.new(noshow_params)
+    @noshow.participant_id = @current_participant.id
+    attendance = Attendance.find_by(task_id: @noshow.task.id, particpant_id: @current_participant)
+    attendance.invitation_state = 2
+    attendance.save
     if @noshow.save
       render json: @noshow
     end
   end
 
-  def update
-    @noshow = Noshow.find params[:id]
-    render json: @noshow
-  end
-
   def noshow_params
-    params.require(:noshow).permit(:participant, :task, :spotted)
+    params.require(:noshow).permit(:spotted, :task)
   end
 end
