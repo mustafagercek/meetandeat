8213501ed745dac232c5617c1947f51dd7b02af2
Mustafa Gercek <mgercek92@googlemail.com>
2018-02-17 22:23:18 +0100

    name

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Meetandeat
     config.load_defaults 5.1
     config.autoload_paths += %W(#{config.root}/lib)
     config.active_record.default_timezone = :local
+    config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
