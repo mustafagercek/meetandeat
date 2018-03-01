668fe1aa87f73c2ef1fe8cd05a30bc62476edb69
Mustafa Gercek <mgercek92@googlemail.com>
2018-03-01 18:31:04 +0100

    fixed changes

--- app/controllers/api/attendances_controller.rb
+++ app/controllers/api/attendances_controller.rb
@@ -15,25 +15,29 @@ class Api::AttendancesController < BaseApiController
 
   def update
     @attendance = Attendance.find params[:id]
-    @attendance.query_state = 2
-    if @attendance.update attendances_params
-      task_requirement = TaskRequirement.find_by(task_id: @attendance.task.id, role_id: @current_participant.role_id)
-      if @attendance.timeslot1
-        task_requirement.timeslot1_acceptance += 1
-        task_requirement.save
+    if @attendance.query_state == 1
+      @attendance.query_state = 2
+      if @attendance.update attendances_params
+        task_requirement = TaskRequirement.find_by(task_id: @attendance.task.id, role_id: @current_participant.role_id)
+        if @attendance.timeslot1
+          task_requirement.timeslot1_acceptance += 1
+          task_requirement.save
 
-      end
-      if @attendance.timeslot2
-        task_requirement.timeslot2_acceptance += 1
-        task_requirement.save
+        end
+        if @attendance.timeslot2
+          task_requirement.timeslot2_acceptance += 1
+          task_requirement.save
 
+        end
+        if @attendance.timeslot3
+          task_requirement.timeslot3_acceptance += 1
+          task_requirement.save
+        end
       end
-      if @attendance.timeslot3
-        task_requirement.timeslot3_acceptance += 1
-        task_requirement.save
-      end
+      render json: @attendance, status: 200
     end
-    render json: @attendance, status: 200
+  else
+    render json: {}, status: 400
   end
 
   def attendances_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@ kitchen5 = Kitchen.create(name: 'Diner')
 
 role1 = Role.create(name: 'Management')
 role3 = Role.create(name: 'Other')
+
+participant1 = Participant.create(email: '1', password: '1', role_id: role1.id)
+participant2 = Participant.create(email: '2', password: '1', role_id: role3.id)
+
+task1 = Task.create(title: 'title', description: 'description', survey_start: Time.new(2015, 12, 8, 10, 19), survey_end: Time.new(2025, 12, 8, 10, 19), algorithm: 0, kitchen_id: kitchen1.id)
+
+taskRequirement1 = TaskRequirement.create(task_id: task1.id, role_id: role1.id, number: 3)
+taskRequirement1 = TaskRequirement.create(task_id: task1.id, role_id: role3.id, number: 3)
+
